4c2e9ede4ecf566c4c0c0a8bf7e778bb5af9f454
Alex Black <wa7@sa34.cx>
2021-02-22 21:56:33 +0300

    Done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Print temperature in Fahrenheit");
+
+    let mut temperature = String::new();
+
+    io::stdin()
+        .read_line(&mut temperature)
+        .expect("Failed to read line");
+
+    let mut temperature: f32 = temperature
+        .trim()
+        .parse()
+        .expect("Please type a number");
+
+    // (0°F − 32) × 5/9 = -17.78°C
+
+    temperature = fahrenheit_to_celsius(temperature);
+    println!("{}°F", temperature);
+}
+
+fn fahrenheit_to_celsius (temperature: f32) -> f32 {
+    (temperature - 32.0) * 5.0 / 9.0
 }
